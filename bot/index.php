7c62846e07895657c000a43e83a7893cc84c2da0
Aleksandr Novikov <pwsdotru@gmail.com>
2026-06-28 20:24:57 +0300

    Show Image for 3 days

--- bot/index.php
+++ bot/index.php
@@ -1,5 +1,6 @@
 <?php
 
+const days_to_show = 3;
 require_once(dirname(__DIR__) . "/vendor/autoload.php");
 
 use TelegramBot\Api\Client;
@@ -19,12 +20,12 @@ try {
 
     $bot->command('show', function ($message) use ($bot) {
             global $config;
-            $text = sprintf("Tides from %s for 7 days", date("Y-m-d"));
+            $text = sprintf("Tides from %s for %d days", date("Y-m-d"), days_to_show);
             try {
                 $tides = new API($config["WORLDTIDES_APIKEY"]);
                 $tides->setDate(date("Y-m-d"))
                     ->setPoint($config["POINT_LAT"], $config["POINT_LON"]);
-                $img = $tides->getImage(7);
+                $img = $tides->getImage(days_to_show);
                 $tmpfile = tempnam("/tmp", "tides_");
                 file_put_contents($tmpfile, $img);
                 $photo = new \CURLFile($tmpfile);
